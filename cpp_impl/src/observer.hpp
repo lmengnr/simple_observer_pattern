a205518fc49e4692f4522b0fd88d7def4c2c1813
lmengnr <fahd.imtiaz.bhatti@gmail.com>
2021-12-18 15:48:40 +0500

    created the concrete implementation and refined structure

--- cpp_impl/src/observer.hpp
+++ cpp_impl/src/observer.hpp
@@ -1,3 +1,7 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+
 
 struct ActivityData
 {
@@ -19,5 +23,43 @@ public:
     IObserver(/* args */){};
     IObserver(){};
 
-    virtual void Update(ActivityData& newState);
+    virtual void Update(const ActivityData& newState);
+};
+
+
+class cObserverA : IObserver
+{
+private:
+    /* data */
+public:
+    cObserverA(/* args */){};
+    ~cObserverA(){};
+
+    void Update(const ActivityData& newState) override
+    {
+        std::cout << "New Position: x = " << newState.xpos << ", y = " <<newState.ypos << std::endl;
+
+    }
+
+};
+
+class cDistObserver : IObserver
+{
+private:
+    ActivityData oPrevPos;
+public:
+
+    cDistObserver(const ActivityData& initPos)
+    {
+        oPrevPos = initPos;
+    }
+
+    void Update(const ActivityData& newState) override
+    {
+        double distance = sqrt( 
+            ( pow( newState.xpos - oPrevPos.xpos, 2) + pow( newState.ypos - oPrevPos.ypos, 2)
+            ));
+
+        std::cout << "Distance travelled: " << distance << std::endl;
+    }
 };
--- cpp_impl/src/subject.hpp
+++ cpp_impl/src/subject.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "observer.hpp"
 
 #include <map>
